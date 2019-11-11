9f6c4fef2d7f4896546918286724c26aef408631
sxw1 <53978912+sxw1@users.noreply.github.com>
2019-11-10 21:10:06 -0800

    revised database.py

--- common/database.py
+++ common/database.py
@@ -5,7 +5,7 @@ import pymongo
 
 class Database:
     URI = os.environ.get("MONGOLAB_URI")
-    DATABASE = pymongo.MongoClient(URI).get_database()
+    DATABASE = pymongo.MongoClient(URI).get_default_database()
 
     @staticmethod
     def insert(collection: str, data: Dict) -> None:
